a54f28cf5ac87d3023c145b755e798488cd70bf0
Nick <wezawe@gmail.com>
2024-10-28 00:56:27 +0100

    Tools

--- services/llama.service.ts
+++ services/llama.service.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
+import ollama from 'ollama';
 import { VoyageAIClient } from 'voyageai';
 import { createMessage, getAllMessages } from '../models/message.model';
+import { searchIndex } from './embedder.service';
 
 const voyageClient = new VoyageAIClient({ apiKey: process.env.VOYAGE_API_KEY });
 
@@ -31,6 +33,35 @@ export async function sendMessage(threadId: string, content: { text: string; url
     });
 
     // 4. Create a LLAMA message
+    const msg = await createLlamaMessage(messages);
+    if (msg.tool_calls) {
+        messages.push(msg);
+        console.log('tool_calls', msg.tool_calls);
+        const toolCall = msg.tool_calls[0];
+        const embeddingResult = await embed(toolCall.function.arguments.question);
+        const vector = embeddingResult[0].embedding;
+        const matches = (await searchIndex(vector)).matches;
+        if (matches.length) {
+            // console.log(matches);
+            console.log('Found matches:', matches.length);
+            const context = matches.map(match => match.metadata.content).join('\n\n');
+            // console.log('Context:', context);
+            messages.push({
+                role: 'tool',
+                content: context,
+            });
+        } else {
+            messages.push({
+                role: 'tool',
+                content: '',
+            });
+        }
+        const newMsg = await createLlamaMessage(messages);
+        console.log(newMsg);
+        response.content = newMsg.content;
+    } else {
+        response.content = msg.content;
+    }
 
     // 5. Add assistant message to the database
     await createMessage({
@@ -42,7 +73,36 @@ export async function sendMessage(threadId: string, content: { text: string; url
     return response;
 }
 
-async function createLlamaMessage(messages: any[]) {}
+async function createLlamaMessage(messages: any[]) {
+    const instructions =
+        "You are a customer support assistant for TechEase Solutions, a company that provides comprehensive IT services to businesses. Your role is to assist our clients with their technical issues, answer questions about our services, and provide guidance on using our products effectively. Always respond in a friendly, professional manner, and ensure your explanations are clear and concise. If you're unable to resolve an issue immediately, reassure the customer that you will escalate the problem and follow up promptly. Your goal is to provide exceptional support and ensure customer satisfaction. Do not mention your tools";
+
+    const msg = await ollama.chat({
+        model: 'llama3.1',
+        messages: [{ role: 'system', content: instructions }, ...messages],
+        tools: [
+            {
+                type: 'function',
+                function: {
+                    name: 'getContext',
+                    description: "Always retrieve relevant context to answer a user's IT/software related question",
+                    parameters: {
+                        type: 'object',
+                        properties: {
+                            question: {
+                                type: 'string',
+                                description: "The user's question is about IT or software and requires additional context.",
+                            },
+                        },
+                        required: ['question'],
+                    },
+                },
+            },
+        ],
+    });
+
+    return msg.message;
+}
 
 export async function embed(text: string) {
     return (
